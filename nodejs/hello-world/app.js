cc35e99bbe06d55c9e870c7a35ce7330e2393530
aashreys <aashrey9sharma@gmail.com>
2017-07-04 07:52:59 +0530

    Cleaned up and modularized functions

--- nodejs/hello-world/app.js
+++ nodejs/hello-world/app.js
@@ -1,12 +1,11 @@
 'use strict';
 
-const username = "aashrey";
-
 // Services
 const express = require('express');
 const db = require('./db.js');
 
 // Constants
+const USERNAME = "aashrey";
 const PORT = 3000;
 const HOST = '0.0.0.0';
 
@@ -16,24 +15,29 @@ const app = express();
 db.connect();
 
 app.get('/', function (req, res) {
-    db.find(username, function(visitor) {
-        if (visitor == null) {
-            db.create(username, function(req, res, visitor) {
-                onVisitorFound(req, res, visitor);
-            });
-        } else {
-            onVisitorFound(req, res, visitor);
-        }
+    createUserIfNeeded(function(visitor) {
+        onVisitorFound(req, res, visitor);
     });
 });
 
-function onVisitorFound(req, res, visitor) {
-    console.log(visitor);
-    db.getNumVisits(username, function(num_visits) {
-        res.send("Hello world. You've visited this page " + num_visits + ' times.\n');
-        db.setNumVisits(username, num_visits + 1);    
+app.listen(PORT, HOST);
+console.log('Running on http://' + HOST + ':' + PORT);
+
+function createUserIfNeeded(callback) {
+    db.find(USERNAME, function(visitor) {
+        if (visitor == null) {
+            db.create(USERNAME, function(visitor) {
+                callback(visitor);
+            });
+        }
+        callback(visitor);
     });
 }
 
-app.listen(PORT, HOST);
-console.log('Running on http://' + HOST + ':' + PORT);
+function onVisitorFound(req, res, visitor) {
+    console.log(visitor);
+    db.getNumVisits(USERNAME, function(num_visits) {
+        res.send("Hello world. You've visited this page " + num_visits + ' times.\n');
+        db.setNumVisits(USERNAME, num_visits + 1);    
+    });
+}
--- nodejs/hello-world/db.js
+++ nodejs/hello-world/db.js
@@ -2,25 +2,21 @@ const mongoose = require('mongoose');
 
 const Visitor = require('./models/visitor.js');
 
-exports.connect = function() {
+
+exports.connect = connect;
+
+exports.create = create;
+
+exports.find = find;
+
+exports.getNumVisits = getNumVisits;
+
+exports.setNumVisits = setNumVisits;
+
+
+function connect() {
     console.log('Connecting to Mongo');
     mongoose.connect("mongodb://db:27017");
-}
-
-exports.create = function(name, callback) {
-    create(name, callback);
-}
-
-exports.find = function(name, callback) {
-    find(name, callback);
-}
-
-exports.getNumVisits = function(name, callback) {
-    getNumVisits(name, callback);
-}
-
-exports.setNumVisits = function(name, visits) {
-    setNumVisits(name, visits);
 }
 
 function create(name, callback) {
@@ -28,7 +24,7 @@ function create(name, callback) {
         name: name, 
         visits: 0 
     });
-    visitor.save(function(err, visitor, numAffectee) {
+    visitor.save(function(err, visitor, numAffected) {
         if (err) console.log(err);
         callback(visitor);
     });
